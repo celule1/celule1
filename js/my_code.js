acf46d8dc398af68f796942b149b28388d4311ea
celule1 <162320475+celule1@users.noreply.github.com>
2024-06-18 12:19:18 +0300

    Update my_code.js

--- js/my_code.js
+++ js/my_code.js
@@ -25,21 +25,7 @@ var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             attribution: 'google'
         })
     }, { 'drawlayer': drawnItems }, { position: 'topleft', collapsed: false }).addTo(map);
-    map.addControl(new L.Control.Draw({
-        edit: {
-            featureGroup: drawnItems,
-            poly: {
-                allowIntersection: false
-            }
-        },
-        draw: {
-            polygon: {
-                allowIntersection: false,
-                showArea: true
-            }
-        }
-    }));
-
+  
     map.on(L.Draw.Event.CREATED, function (event) {
         var layer = event.layer;
 
