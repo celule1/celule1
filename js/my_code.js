210edc36a41786398e1820404a2cccd23095fd22
celule1 <162320475+celule1@users.noreply.github.com>
2024-06-18 12:21:28 +0300

    Update my_code.js

--- js/my_code.js
+++ js/my_code.js
@@ -6,34 +6,14 @@ var lon =  parseFloat(urlParams.get('lon'));
 var azimuth = parseInt(urlParams.get('azimuth'));
 var radius = parseInt(urlParams.get('radius'));
 var angle =parseInt(urlParams.get('angle'));
-/*
+
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
 	map.setView(new L.LatLng(lat, lon), 8);
-*/
-//////////////////////
-var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib }),
-            map = new L.Map('map', { center: new L.LatLng(51.505, -0.04), zoom: 13 }),
-            drawnItems = L.featureGroup().addTo(map);
-    L.control.layers({
-        'osm': osm.addTo(map),
-        "google": L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
-            attribution: 'google'
-        })
-    }, { 'drawlayer': drawnItems }, { position: 'topleft', collapsed: false }).addTo(map);
-  
-    map.on(L.Draw.Event.CREATED, function (event) {
-        var layer = event.layer;
 
-        drawnItems.addLayer(layer);
-    });
-
-/////////////////////
-	//setLocation();
+	setLocation();
 
 	function onLocationFound(e) {
 		var radius = e.accuracy / 2;
